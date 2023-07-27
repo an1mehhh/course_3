178108222b81e9d3387f6f051678d59c32716624
an1meh <adolf.ddd@mail.ru>
2023-07-27 08:56:08 +0300

    add convert date to correct format

--- course/main.py
+++ course/main.py
@@ -1,9 +1,15 @@
-from utils.utils import read_file
+from utils.utils import read_file, transfer_date
+
+
+# import datetime
 
 
 def main():
-    operations = read_file("./data/operations.json")
-    print(operations)
+    path = "./data/operations.json"
+    operations = read_file(path)
+    date_ = transfer_date(operations)
+
+    print(date_)
 
 
 if __name__ == "__main__":
--- course/utils/utils.py
+++ course/utils/utils.py
@@ -1,7 +1,20 @@
 import json
+import datetime
 
 
 def read_file(path):
     with open(path, "r", encoding="utf-8") as file:
-        data = json.load(file)
-    return data
+        return json.load(file)
+
+
+def transfer_date(data):
+    date = []
+    for x in data:
+        if x == {}:
+            continue
+        elif x["state"] == "EXECUTED":
+            date_line = datetime.datetime.fromisoformat(x["date"])
+            converted_date = datetime.datetime.strftime(date_line, "%d.%m.%Y")
+            date.append(converted_date)
+    return date[-5:]
+
